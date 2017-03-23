4d6d98a7208c4c8b08ce4e2d66429404e1e903db
satoshicano <satoshicano@gmail.com>
2017-03-23 23:45:47 +0900

    remove padding

--- src/components/InternshipCard.jsx
+++ src/components/InternshipCard.jsx
@@ -3,14 +3,12 @@ import {Card, CardContent} from 'material-ui/Card';
 import Text from 'material-ui/Text';
 
 export default function InternshipCard() {
-  return <div style={{paddingTop: '15px'}}>
-    <Card>
-      <CardContent>
-        <Text type="headline" component="h2">
-          Interhship Experience
-        </Text>
-        <Text type="body1">text</Text>
-      </CardContent>
-    </Card>
-  </div>;
+  return <Card>
+    <CardContent>
+      <Text type="headline" component="h2">
+        Interhship Experience
+      </Text>
+      <Text type="body1">text</Text>
+    </CardContent>
+  </Card>;
 }
